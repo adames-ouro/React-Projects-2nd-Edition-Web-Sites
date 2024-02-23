5d153f1ae42a93c15add7b65b1be29ea72d24f7e
Carlos Adames <adames@ouroanalytics.ai>
2024-02-23 13:33:53 -0400

    modified:   CH1/.txt 	new file:   CH1/babel.config.json 	new file:   CH1/dist/main.js 	modified:   CH1/package-lock.json 	modified:   CH1/package.json 	new file:   CH1/public/index.html 	modified:   CH1/src/index.js 	new file:   CH1/webpack.config.js

--- CH1/src/index.js
+++ CH1/src/index.js
@@ -1,1 +1,9 @@
-console.log('Rick and Morty');
+import ReactDOM from 'react-dom/client';
+
+function App() {
+    return <h1> Rick and Morty </h1>;
+}
+
+const container = document.getElementById('app');
+const root = ReactDOM.createRoot(container);
+root.render(<App />); // mounts app component to an element with the root ID selector in the document (entry point of the app)
