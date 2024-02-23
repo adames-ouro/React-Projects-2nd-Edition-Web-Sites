a2ca44f6cd3751619b0d483214606f55b23bb56d
Carlos Adames <adames@ouroanalytics.ai>
2024-02-23 14:06:02 -0400

    modified:   CH1/.txt 	new file:   CH1/dist/index.html 	modified:   CH1/dist/main.js 	new file:   CH1/dist/main.js.LICENSE.txt 	modified:   CH1/package-lock.json 	modified:   CH1/package.json 	modified:   CH1/public/index.html 	modified:   CH1/src/index.js

--- CH1/src/index.js
+++ CH1/src/index.js
@@ -4,6 +4,6 @@ function App() {
     return <h1> Rick and Morty </h1>;
 }
 
-const container = document.getElementById('app');
+const container = document.getElementById('root'); // same as html file in public folder ->  <section id="root"></section>
 const root = ReactDOM.createRoot(container);
 root.render(<App />); // mounts app component to an element with the root ID selector in the document (entry point of the app)
