5bf79ba59b13e4f82c7755aedc2c5e5a1bd78db8
Carlos Adames <adames@ouroanalytics.ai>
2024-02-24 12:21:35 -0400

    modified:   portfolio_template/src/App.js 	modified:   portfolio_template/src/components/Header.css 	modified:   portfolio_template/src/components/Header.js 	new file:   portfolio_template/src/pages/Profile.js

--- portfolio_template/src/App.js
+++ portfolio_template/src/App.js
@@ -1,11 +1,13 @@
 import Header from './components/Header';
 import './App.css';
 import logo from './assets/logo.svg';
+import Profile from './pages/Profile';
 
 function App() {
   return (
     <div className="App">
       <Header logo={logo}/>
+      <Profile userName='octocat'/>
     </div>
   );
 }
