1b20194b98939a97d7ef6a17e596d24cefcecc9d
Carlos Adames <adames@ouroanalytics.ai>
2024-02-24 11:54:14 -0400

    new file:   portfolio_template/.gitignore 	new file:   portfolio_template/package-lock.json 	new file:   portfolio_template/package.json 	new file:   portfolio_template/public/index.html 	new file:   portfolio_template/src/App.css 	new file:   portfolio_template/src/App.js 	new file:   portfolio_template/src/assets/logo.svg 	new file:   portfolio_template/src/components/Header.css 	new file:   portfolio_template/src/components/Header.js 	new file:   portfolio_template/src/components/Link.css 	new file:   portfolio_template/src/components/Link.js 	new file:   portfolio_template/src/index.css 	new file:   portfolio_template/src/index.js

---
+++ portfolio_template/src/App.js
@@ -0,0 +1,13 @@
+import Header from './components/Header';
+import './App.css';
+import logo from './assets/logo.svg';
+
+function App() {
+  return (
+    <div className="App">
+      <Header logo={logo}/>
+    </div>
+  );
+}
+
+export default App;
