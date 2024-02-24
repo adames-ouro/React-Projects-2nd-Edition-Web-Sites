c64df7226078f754aea4b491425ec765e3c465db
Carlos Adames <adames@ouroanalytics.ai>
2024-02-24 13:08:55 -0400

    modified:   portfolio_template/package-lock.json 	modified:   portfolio_template/package.json 	new file:   portfolio_template/src/components/List.css 	new file:   portfolio_template/src/components/List.js 	modified:   portfolio_template/src/pages/Profile.js

--- portfolio_template/src/pages/Profile.js
+++ portfolio_template/src/pages/Profile.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import Link from '../components/Link.js';
 import './Profile.css';
-
+import List from '../components/List.js';
 
 function Profile({userName}) {
     const [loading, setLoading] = useState(false);
@@ -20,6 +20,39 @@ function Profile({userName}) {
         fetchData();
     } , [userName])
 
+    const items = [
+        {
+            field: 'html_url',
+            value: <Link url={profile.html_url}
+            title={profile.html_url} />,
+        },
+        {
+            field: 'repos_url',
+            value: <Link url={profile.repos_url}
+            title={profile.repos_url} />,
+        },
+        { 
+            field: 'name',
+            value: profile.name 
+        },
+        { 
+            field: 'company', 
+            value: profile.company 
+        },
+        { 
+            field: 'location', 
+            value: profile.location 
+        },
+        { 
+            field: 'email', 
+            value: profile.email 
+        },
+        { 
+            field: 'bio', 
+            value: profile.bio 
+        },
+    ];
+
     return (
         <div className="Profile-container">
             <h2>About me</h2>
@@ -31,45 +64,7 @@ function Profile({userName}) {
                     className='Profile-avatar'
                     src={profile.avatar_url}
                     alt={profile.name}/>
-
-                    <ul>
-                        <li>
-                            <span>html_url: </span>
-                            <Link url={profile.html_url}
-                            title={profile.html_url} />
-                        </li>
-                        
-                        <li>
-                            <span>repos_url: </span>
-                            <Link url={profile.repos_url}
-                            title={profile.repos_url} />
-                        </li>
-                        
-                        <li>
-                            <span>name: </span>
-                            {profile.name}
-                        </li>
-                        
-                        <li>
-                            <span>company: </span>
-                            {profile.company}
-                        </li>
-                        
-                        <li>
-                            <span>location: </span>
-                            {profile.location}
-                        </li>
-                        
-                        <li>
-                            <span>email: </span>
-                            {profile.email}
-                        </li>
-                        
-                        <li>
-                            <span>bio: </span>
-                            {profile.bio}
-                        </li>
-                    </ul>
+                    <List items={items}/>
                 </div>
             )}
         </div>
