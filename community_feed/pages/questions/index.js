5f429095c52e04320bf7f173d6ace90fcec17f2d
Carlos Adames <adames@ouroanalytics.ai>
2024-02-28 14:31:17 -0400

    deleted:    .gitignore 	deleted:    .ipynb 	modified:   community_feed/pages/questions/[id].js 	modified:   community_feed/pages/questions/index.js

--- community_feed/pages/questions/index.js
+++ community_feed/pages/questions/index.js
@@ -1,65 +1,48 @@
-// This part remains the same
 import styled from 'styled-components';
-import Link from 'next/link';
+import {useState, useEffect} from 'react';
 import Card from '../../components/Card';
-import Pagination from '../../components/Pagination';
 
-const QuestionsContainer = styled.div`
+const QuestionsContainer = styled.div` 
   display: flex;
+  justify-content: space-between;
   flex-direction: column;
   margin: 5%;
-  justify-content: space-between;
-`;
+  `;
 
-const CardLink = styled.a`
-  text-decoration: none;
-`;
+  function Questions() {
+    const [loading, setLoading] = useState(true);
+    const [questions, setQuestions] = useState([]);
 
-function Questions({ questions, hasMore, page }) {
-  // No need to fetch the data in a useEffect hook anymore
-  // The data is now passed as a prop to the component
+    useEffect(() => {
+      async function fetchData() {
+        const data = await fetch(`https://api.stackexchange.com/2.2/questions?order=desc&sort=hot&tagged=reactjs&site=stackoverflow`);
+        const result = await data.json();
+        if (result) {
+          setQuestions(result.items);
+          setLoading(false);
+        }
+      }
+      fetchData();
+    }, []);
 
-  return (
+    return (
     <QuestionsContainer>
-      <h2>Questions</h2>
-      {questions ? (
+      <h1>Questions</h1>
+      {loading ? (
         <span>Loading...</span>
-      ) : (
-        <>
-          <div>
-            { questions && questions.map((question) => (
-              <Link key={question.question_id} href={`/questions/${question.question_id}`} passHref>
-                <CardLink>
-                  <Card
-                    title={question.title}
-                    views={question.view_count}
-                    answers={question.answer_count}/>
-                </CardLink>
-              </Link>
-            ))}
-          </div>
-          <Pagination currentPage={parseInt(page) || 1} hasMore={hasMore} />
-        </>
+      ):(
+        <div>
+          {questions.map((question) => (
+            <Card 
+            key={question.question_id} 
+            title={question.title} 
+            views={question.view_count} 
+            answers={question.answer_count}/>
+          ))}
+        </div>
       )}
     </QuestionsContainer>
-  );
-}
+      );
+  }
 
-// Fetch the data on the server side before the page is rendered
-export async function getServerSideProps(context) {
-  const { page } = context.query;
-  const res = await fetch(`https://api.stackexchange.com/2.2/questions?${page ? `page=${page}&` : ''}order=desc&sort=hot&tagged=reactjs&site=stackoverflow`);
-  const data = await res.json();
-  const questions = data.items;
-  const hasMore = data.has_more;
-
-  return {
-    props: {
-      questions,
-      hasMore,
-      page,
-    },
-  };
-}
-
-export default Questions;
+  export default Questions;
